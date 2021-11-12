66b7a374aa120e528d519b14a79094cdeca3a894
Rebecca <rebecca@heyman.com>
2021-11-11 19:57:34 -0600

    remove unnecessary files

--- config/dev-sample.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    mongo_uri : 'insert mongo URI',
-    db: "database",
-    AWS_BUCKET_NAME: 'AWS bucket name',
-    AWS_BUCKET_REGION: 'AWS region',
-    AWS_ACCESS_KEY: 'enter Access Key',
-    AWS_SECRET_KEY: 'Enter Secret Key'
-};
